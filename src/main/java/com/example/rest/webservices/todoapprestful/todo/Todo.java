0dd3e6fa9469a7a18d1a24d8dc4bba0e6a713f4c
TomaEduard <eduard.daniel.toma@gmail.com>
2019-10-18 11:07:28 +0300

    added CRUD opperations Post-uri, Delete-noContent, Put-ResponseEntity

--- src/main/java/com/example/rest/webservices/todoapprestful/todo/Todo.java
+++ src/main/java/com/example/rest/webservices/todoapprestful/todo/Todo.java
@@ -10,12 +10,37 @@ public class Todo {
 	private Date targetDate;
 	private boolean isDone;
 
+	public Todo() {
+	}
+
 	public Todo(long id, String username, String description, Date targetDate, boolean isDone) {
 		this.id = id;
 		this.username = username;
 		this.description = description;
 		this.targetDate = targetDate;
 		this.isDone = isDone;
+	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (int) (id ^ (id >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Todo other = (Todo) obj;
+		if (id != other.id)
+			return false;
+		return true;
 	}
 
 	public long getId() {
--- src/main/java/com/example/rest/webservices/todoapprestful/todo/TodoHardcodedService.java
+++ src/main/java/com/example/rest/webservices/todoapprestful/todo/TodoHardcodedService.java
@@ -14,11 +14,45 @@ public class TodoHardcodedService {
 
 	static {
 		todos.add(new Todo(++idCounter, "newName", "Learn To Dance", new Date(), false));
-		todos.add(new Todo(++idCounter, "newName", "Learn about Microservices", new Date(), false));
+		todos.add(new Todo(++idCounter, "newName", "Learn about Microservices 2", new Date(), false));
 		todos.add(new Todo(++idCounter, "newName", "Learn about Angular", new Date(), false));
 	}
 
 	public List<Todo> findAll() {
 		return todos;
+	}
+	
+	public Todo saveAndUpdate(Todo todo) {
+		// new todo have id -1 or 0
+		if(todo.getId()== -1 || todo.getId()== 0) {
+			todo.setId(++idCounter);
+			todos.add(todo);
+		} else {
+			deleteById(todo.getId());
+			todos.add(todo);
+		}
+		return todo;
+	}
+	
+	public Todo deleteById(long id) {
+		Todo todo = findById(id);
+		if (todo == null) {
+			return null;
+		}
+		
+		if(todos.remove(todo)) {
+			return todo;
+		}
+		
+		return null;
+	}
+
+	public Todo findById(long id) {
+		for(Todo todo:todos) {
+			if(todo.getId() == id) {
+				return todo;
+			}
+		}
+		return null;
 	}
 }
